ff85e4331987dfed31c1d72e2bf1725a57b65992
Shirasaka-Takahiro <kenshirou913@gmail.com>
2022-09-04 17:47:18 +0900

    edited file

--- mysql/init_mysql.sh
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE 'wp_test_db';
-CREATE USER 'wp_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Wordpress1!';
-GRANT ALL PRIVILEGES on wp_test_db.* TO 'wp_user'@'localhost';
